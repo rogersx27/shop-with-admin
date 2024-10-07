5e1d01f862e9720a66d2fe562771a560e0a63c9e
Juan Pablo Guevara López <juanpabloguevara8b@gmail.com>
2024-10-07 15:50:38 -0500

    añade router para category

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from models import Base
-from crud import category, productDetail, product, orderItem, order, customer
 
-from database import engine, get_db
+from routes.models import categoryRouter
 
 templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.include_router(categoryRouter.router, prefix="/api")
 
 
 @app.get("/")
--- app/routes/models/categoryRouter.py
+++ app/routes/models/categoryRouter.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
-from app import schemas, crud, models
+import schemas
+import crud.category as crud
 from database import get_db
 
 router = APIRouter()
@@ -9,24 +10,24 @@ router = APIRouter()
 
 @router.post("/categories/", response_model=schemas.CategoryResponse)
 def create_category(category: schemas.CategoryCreate, db: Session = Depends(get_db)):
-    return crud.category.create_category(db=db, category=category)
+    return crud.create_category(db=db, category=category)
 
 
 @router.get("/categories/", response_model=List[schemas.CategoryResponse])
 def get_categories(db: Session = Depends(get_db)):
-    return crud.category.get_categories(db=db)
+    return crud.get_categories(db=db)
 
 
 @router.get("/categories/{category_id}/", response_model=schemas.CategoryResponse)
 def get_category(category_id: str, db: Session = Depends(get_db)):
-    return crud.category.get_category_by_id(db=db, category_id=category_id)
+    return crud.get_category_by_id(db=db, category_id=category_id)
 
 
 @router.put("/categories/{category_id}/", response_model=schemas.CategoryResponse)
 def update_category(category_id: str, category: schemas.CategoryUpdate, db: Session = Depends(get_db)):
-    return crud.category.update_category(db=db, category_id=category_id, category_update=category)
+    return crud.update_category(db=db, category_id=category_id, category_update=category)
 
 
 @router.delete("/categories/{category_id}/")
 def delete_category(category_id: str, db: Session = Depends(get_db)):
-    return crud.category.delete_category(db=db, category_id=category_id)
+    return crud.delete_category(db=db, category_id=category_id)
