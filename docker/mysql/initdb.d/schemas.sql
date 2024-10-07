c6eb87aec4e73766c3028ee57e12534932a6831b
Juan Pablo Guevara López <juanpabloguevara8b@gmail.com>
2024-10-07 14:16:02 -0500

    añade nuevos esquemas de db

--- docker/mysql/initdb.d/schemas.sql
+++ docker/mysql/initdb.d/schemas.sql
@@ -1,47 +1,66 @@
--- CREATE TABLE
---     categories (
---         id CHAR(36) PRIMARY KEY,
---         name VARCHAR(255) NOT NULL
---     );
+CREATE TABLE
+    categories (
+        id CHAR(36) PRIMARY KEY NOT NULL DEFAULT 'UUID()',
+        name VARCHAR(255) NOT NULL UNIQUE
+    );
 
--- CREATE TABLE
---     customers (
---         id CHAR(36) PRIMARY KEY,
---         name VARCHAR(255) NOT NULL,
---         email VARCHAR(255) UNIQUE NOT NULL,
---         address VARCHAR(255),
---         phone VARCHAR(50)
---     );
+CREATE TABLE
+    customers (
+        id CHAR(36) PRIMARY KEY NOT NULL DEFAULT 'UUID()',
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        address VARCHAR(255),
+        phone VARCHAR(50)
+    );
 
--- CREATE TABLE
---     products (
---         id CHAR(36) PRIMARY KEY,
---         name VARCHAR(255) NOT NULL,
---         category_id CHAR(36) NOT NULL,
---         price DECIMAL(10, 2) NOT NULL,
---         description TEXT,
---         image_url VARCHAR(255),
---         quantity VARCHAR(50),
---         availability BOOLEAN DEFAULT TRUE,
---         FOREIGN KEY (category_id) REFERENCES categories (id)
---     );
+CREATE TABLE
+    products (
+        id CHAR(36) PRIMARY KEY NOT NULL DEFAULT 'UUID()',
+        generic_name VARCHAR(255) NOT NULL,
+        category_id CHAR(36) NOT NULL,
+        description TEXT,
+        image_url VARCHAR(255),
+        availability BOOLEAN DEFAULT TRUE,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        FOREIGN KEY (category_id) REFERENCES categories (id)
+    );
 
--- CREATE TABLE
---     orders (
---         id CHAR(36) PRIMARY KEY,
---         customer_id CHAR(36) NOT NULL,
---         order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
---         status VARCHAR(50) NOT NULL,
---         FOREIGN KEY (customer_id) REFERENCES customers (id)
---     );
+CREATE TABLE
+    product_details (
+        id CHAR(36) PRIMARY KEY NOT NULL DEFAULT 'UUID()',
+        product_id CHAR(36) NOT NULL,
+        brand_name VARCHAR(255) NOT NULL,
+        strength VARCHAR(50) NOT NULL,
+        composition TEXT,
+        supply_type VARCHAR(50) NOT NULL,
+        manufacturer VARCHAR(255) NOT NULL,
+        other_brand_names TEXT,
+        price DECIMAL(10, 2) NOT NULL,
+        stock INT NOT NULL,
+        FOREIGN KEY (product_id) REFERENCES products (id)
+    );
 
--- CREATE TABLE
---     order_items (
---         id CHAR(36) PRIMARY KEY,
---         order_id CHAR(36) NOT NULL,
---         product_id CHAR(36) NOT NULL,
---         quantity INT NOT NULL,
---         price DECIMAL(10, 2) NOT NULL,
---         FOREIGN KEY (order_id) REFERENCES orders (id),
---         FOREIGN KEY (product_id) REFERENCES products (id)
---     );
+CREATE TABLE
+    orders (
+        id CHAR(36) PRIMARY KEY NOT NULL DEFAULT 'UUID()',
+        customer_id CHAR(36) NOT NULL,
+        order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+        status VARCHAR(50) NOT NULL,
+        total_amount DECIMAL(10, 2) DEFAULT 0,
+        payment_status VARCHAR(50),
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        FOREIGN KEY (customer_id) REFERENCES customers (id)
+    );
+
+CREATE TABLE
+    order_items (
+        id CHAR(36) PRIMARY KEY NOT NULL DEFAULT 'UUID()',
+        order_id CHAR(36) NOT NULL,
+        product_id CHAR(36) NOT NULL,
+        quantity INT NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        FOREIGN KEY (order_id) REFERENCES orders (id),
+        FOREIGN KEY (product_id) REFERENCES products (id)
+    );
