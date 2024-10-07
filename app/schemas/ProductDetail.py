f8720cc317d9e3bb425a77f78520e6621675f787
Juan Pablo Guevara López <juanpabloguevara8b@gmail.com>
2024-10-07 14:20:54 -0500

    añade esquema para productDetail

---
+++ app/schemas/ProductDetail.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from uuid import UUID
+
+class ProductDetailBase(BaseModel):
+    product_id: UUID
+    brand_name: str
+    strength: str
+    composition: str | None = None
+    supply_type: str
+    manufacturer: str
+    other_brand_names: str | None = None
+    price: float
+    stock: int
+
+
+class ProductDetailCreate(ProductDetailBase):
+    pass
+
+
+class ProductDetailResponse(ProductDetailBase):
+    id: UUID
+
+    class Config:
+        from_attributes = True
