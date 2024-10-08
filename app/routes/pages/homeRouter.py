1d09b73f99a8ca8d89eaba02cbe299aca645ebd3
Juan Pablo Guevara López <juanpabloguevara8b@gmail.com>
2024-10-08 17:29:39 -0500

    añade nuevas rutas html

--- app/routes/pages/homeRouter.py
+++ app/routes/pages/homeRouter.py
@@ -3,7 +3,8 @@ from fastapi.responses import HTMLResponse
 from sqlalchemy.orm import Session
 from fastapi.templating import Jinja2Templates
 from database import get_db
-import services.category as service
+import services.category
+import services.product
 
 templates = Jinja2Templates(directory="templates")
 router = APIRouter(tags=["Pages"])
@@ -11,6 +12,23 @@ router = APIRouter(tags=["Pages"])
 
 @router.get("/home/", response_class=HTMLResponse)
 def get_categories(request: Request, db: Session = Depends(get_db)):
-    category_responses = service.get_categories_with_products(db=db)
+    category_responses = services.category.get_categories_with_products(db=db)
+    letters = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
+    best_sellers = services.product.get_best_sellers_by_field(db=db)
+    
+    context = {
+        "request": request,
+        "categories": category_responses,
+        "letters": letters,
+        "best_sellers": best_sellers
+    }
 
-    return templates.TemplateResponse("index.html", {"request": request, "categories": category_responses})
+    return templates.TemplateResponse("index.html", context)
+
+
+@router.get("/products/start/{letter}/", response_class=HTMLResponse)
+def get_products_by_letter(request: Request, letter: str, db: Session = Depends(get_db)):
+    product_with_letter = services.product.get_all_products_by_first_letter(
+        db=db, letter=letter)
+
+    return templates.TemplateResponse("productsLetter.html", {"request": request, "products": product_with_letter, "letter": letter})
