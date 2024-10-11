01c73f9bf99012a68ba37ac7b4259af011525c13
root <root@server1.coinzra.com>
2024-10-11 21:38:50 +0000

    database

--- app/database.py
+++ app/database.py
@@ -3,6 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
 from sqlalchemy.orm import sessionmaker
 import os
 
+from dotenv import load_dotenv
+
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
+
+
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_HOST = os.getenv("MYSQL_HOST")
